743779abdfaf063be08b00074159da62be64a98a
mattwelter <welter.m@icloud.com>
2024-01-26 13:28:27 -0700

    Something different

--- pages/api/polls.js
+++ pages/api/polls.js
@@ -1,23 +1,7 @@
 // pages/api/polls.js
 
 export default function handler(req, res) {
-  // Handle GET requests
-  if (req.method === 'GET') {
-    // Set Content-Type to text/html
-    res.setHeader('Content-Type', 'text/html');
-
-    // Create a string of HTML content
-    const htmlContent = `
-      <meta property="fc:frame" content="vNext" />
-      <meta property="fc:frame:image" content="https://farcaster-embeds.vercel.app/start.png" />
-      <meta property="fc:frame:button:1" content="Red" />
-      <meta property="fc:frame:button:2" content="Blue" />
-      <meta property="fc:frame:post_url" content="https://farcaster-embeds.vercel.app/api/polls" />
-    `;
-
-    // Send the HTML content
-    res.status(200).send(htmlContent);
-  } else if (req.method === 'POST') {
+   if (req.method === 'POST') {
     res.setHeader('Content-Type', 'text/html');
 
     // Create a string of HTML content
