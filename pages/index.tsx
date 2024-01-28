85da57993ee305fbc4d8003e9504790415a1e7d0
mattwelter <welter.m@icloud.com>
2024-01-27 21:03:34 -0700

    Testing from Greg's repo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,18 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-import Head from 'next/head';
-
-const inter = Inter({ subsets: ["latin"] });
+import Head from 'next/head'
+import { BASE_URL } from './utils'
 
 export default function Home() {
   return (
-    <main>
+    <>
       <Head>
+        <meta property="og:title" content="Frame" />
+        <meta property="og:image" content={`${BASE_URL}/start.png`} />
         <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content="https://farcaster-embeds.vercel.app/start.png" />
-        <meta property="fc:frame:button:1" content="Red" />
-        <meta property="fc:frame:button:2" content="Blue" />
-        <meta property="fc:frame:post_url" content="https://farcaster-embeds.vercel.app/api/polls" />
-
-        <meta property="og:title" content="TEST" />
-        <meta property="og:image" content="https://farcaster-embeds.vercel.app/start.png" />
+        <meta property="fc:frame:image" content={`${BASE_URL}/start.png`} />
+        <meta property="fc:frame:button:1" content="Yes" />
+        <meta property="fc:frame:button:2" content="No" />
+        <meta property="fc:frame:post_url" content={`${BASE_URL}/api/polls`} />
       </Head>
-    </main>
-  );
+    </>
+  )
 }
